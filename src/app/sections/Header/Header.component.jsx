22e250fd343672e2bb87b594f3dd090f3ab6f298
anas boumediene <anexbm@gmail.com>
2024-07-27 02:27:41 +0200

    save

--- src/app/sections/Header/Header.component.jsx
+++ src/app/sections/Header/Header.component.jsx
@@ -1,6 +1,13 @@
+'use client';
+
+import { MdClose, MdMenu } from 'react-icons/md';
 import styles from './Header.module.css';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 export default function Header() {
+    const [menu, setMenu] = useState(false);
+
 	return (
 		<header className={styles.header}>
 			<nav className={styles.nav}>
@@ -16,6 +23,9 @@ export default function Header() {
 						<a href='#contact'>Kontaktieren</a>
 					</li>
 				</ul>
+            <button className={classNames('btn btn-icon', styles.btnNav)} onClick={() => setMenu(!menu) }>
+                {!menu ? <MdMenu size={18} /> : <MdClose size={18} />}
+            </button>
 			</nav>
 		</header>
 	);
